acd9ec6342f1e7fce7ffcddf9ae92656b3a73fd4
AbduSomad <abdusomadkoshmamatov@gmail.com>
2022-10-25 10:28:52 +0600

    commit

---
+++ src/method1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class method1 {
+    public static void main(String[] args) {
+
+myVarargs("Fati", "zaure");
+    }
+
+    public static void myVarargs(String ... name) {
+        System.out.println("lublu => "+Arrays.toString(name));
+
+    }
+    }
